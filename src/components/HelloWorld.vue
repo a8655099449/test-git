87dacb7f7f94d9abf4333cafbd3709e3ba6313d1
kuangw <kuangw96@gmail.com>
2023-11-09 23:21:20 +0800

    m

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -15,7 +15,7 @@ const count = ref(0)
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
       Edit
-      <code>components/HelloWorld.vue</code> to test HMR8888
+      <code>components/HelloWorld.vue</code> to test HMR8888master change
     </p>
   </div>
 
